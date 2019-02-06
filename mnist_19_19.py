0f1df5a14372c7b6d88ee8e3db775122642be764
michal <michal.nand@gmail.com>
2019-02-06 09:54:08 +0100

    common

--- mnist_19_19.py
+++ mnist_19_19.py
@@ -3,6 +3,7 @@ import libs_rysy_python.rysy as rysy
 #load generic images dataset, use dataset_mnist_19_19.json for configuration
 dataset = rysy.DatasetImages("dataset_mnist_19_19.json");
 
+'''
 #use net 0 parameters in experiment
 experiment0 = rysy.ClassificationExperiment(dataset, "networks/net_0/")
 experiment0.run()
@@ -14,6 +15,15 @@ experiment1.run()
 #use net 2 parameters in experiment
 experiment2 = rysy.ClassificationExperiment(dataset, "networks/net_2/")
 experiment2.run()
+'''
+
+#use net 3 parameters in experiment
+experiment3 = rysy.ClassificationExperiment(dataset, "networks/net_3/")
+experiment3.run()
+
+#use net 4 parameters in experiment
+experiment4 = rysy.ClassificationExperiment(dataset, "networks/net_4/")
+experiment4.run()
 
 
 print("program done")
