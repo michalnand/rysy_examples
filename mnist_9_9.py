3838225433a4144dce339d44d7a58480b62eb8cd
michal <michal.nand@gmail.com>
2019-02-06 11:22:08 +0100

    common

--- mnist_9_9.py
+++ mnist_9_9.py
@@ -3,10 +3,10 @@ import libs_rysy_python.rysy as rysy
 #load generic images dataset, use dataset_mnist_9_9.json for configuration
 dataset = rysy.DatasetImages("dataset_mnist_9_9.json");
 
+
 #use net 0 parameters in experiment 0
 experiment_0 = rysy.ClassificationExperiment(dataset, "networks/net_0/")
 experiment_0.run()
-
 
 #use net 1 parameters in experiment 1
 experiment_1 = rysy.ClassificationExperiment(dataset, "networks/net_1/")
